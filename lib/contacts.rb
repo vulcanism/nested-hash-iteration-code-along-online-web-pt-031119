d4b297f6405dffb463814434b74938bdd99b6796
Savannah C <sgcorrell@gmail.com>
2019-03-21 18:28:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice cream| ice_cream == "strawberry"}
+      end
     end
   end
 end
